b743ff5f66b1b121df1cf926c62a7f026faf6eeb
CecileSC <ceciliasinghcaro@gmail.com>
2024-06-19 19:26:35 +0200

    subo cambios con clear, clases y funciones

--- piedra_papel_tijera/piedra-papel-tijera.py
+++ piedra_papel_tijera/piedra-papel-tijera.py
@@ -1,91 +1,114 @@
 
 # Importa la biblioteca random para generar elecciones aleatorias
+
 import random 
 import os
 
-os.system("clear")
-# variable para controlar si se debe volver a jugar
-volver_a_jugar = 's' 
-
-# Bucle principal que se ejecuta mientras el jugador quiera volver a jugar
-while volver_a_jugar == 's':
-
-# Inicializa los contadores de partidas ganadas para el jugador y la máquina
-    partidas_ganadas_jugador = 0
-
-    partidas_ganadas_maquina = 0
-
+class PiedraPapelTijera:
     
-# Bucle que se ejecuta hasta que el jugador o la máquina ganen 3 partidas
+    def __init__(self) -> None:
+        self.mostrar_reglas()
+        
+        pass
 
-    while partidas_ganadas_jugador < 3 and partidas_ganadas_maquina < 3:
+    def mostrar_reglas(self):
+
+        os.system("clear")
+        print(" Piedra ✊ Papel 🖐️  & Tijera ✌️\n")
+
+        print("- Reglas del Juego Piedra, Papel y Tijera:\n")
+
+        print("     - Dos jugadores eligen una de las tres opciones: Piedra, Papel o Tijera.\n")
+
+        print("- Las reglas para determinar al ganador son:\n")
+                
+        print("     - Piedra vence a Tijera.\n")
+
+        print("     - Tijera vence a Papel.\n")
+
+        print("     - Papel vence a Piedra.\n")
+
+        print("     - El jugador que gana una ronda obtiene un punto.\n")
+                
+        print("     - El juego continúa hasta que un jugador haya acumulado 3 puntos, convirtiéndose en el ganador.\n")
+
+    def jugar_partida(self):
+
+        # Inicializa los contadores de partidas ganadas para el jugador y la máquina
+        partidas_ganadas_jugador = 0
+
+        partidas_ganadas_maquina = 0
+
+        # Bucle que se ejecuta hasta que el jugador o la máquina ganen 3 partidas
+
+        while partidas_ganadas_jugador < 3 and partidas_ganadas_maquina < 3:
 
         #El jugador elije su opción
         
-        jugador = input('''introduce:
-                1 si quieres ✊
-                2 si quieres 🖐️
-                3 si quieres ✌️  
-                ''')
+            jugador = input("Introduce 1 si quieres ✊, 2 si quieres 🖐️, 3 si quieres ✌️: ")
         
         #Cambiamos el valor de la variable de la elección del jugador (1, 2, 3) a (✊, 🖐️, ✌️)
 
-        if jugador == "1":
-            jugador = "✊"
+            if jugador == "1":
+                jugador = "✊"
 
-        elif jugador == "2":
-            jugador = "🖐️"
+            elif jugador == "2":
+                jugador = "🖐️"
 
-        elif jugador == "3":
-            jugador = "✌️"
+            elif jugador == "3":
+                jugador = "✌️"
 
- # Si el jugador no elige una opción válida, muestra un mensaje y vuelve a pedirle la entrada
+        # Si el jugador no elige una opción válida, muestra un mensaje y vuelve a pedirle la entrada
         
+            else:
+                print("No has elegido opción correcta ❌❌\n")
+        
+                continue  # Salta el resto del bucle y vuelve a pedir la entrada del jugador
+
+            #La maquina elige su opción de forma aleatoria
+
+            maquina = ("✊", "🖐️", "✌️")
+            maquina = random.choice(maquina)
+
+            #Hacemos las comparativas para ver quien ha ganado
+
+            #Las opciones donde el jugador gana:
+
+            if (maquina == "✊" and jugador == "🖐️") or (maquina == "🖐️" and jugador == "✌️") or (maquina == "✌️" and jugador == "✊"):
+                
+                partidas_ganadas_jugador = partidas_ganadas_jugador +1 # Incrementa el contador de partidas ganadas del jugador
+
+                print("el 🙋‍♀️ ha sacado",jugador, " y la 🤖", maquina,"ha ganado el 🙋‍♀️\n")       
+
+            #Las opciones donde la máquina gana:
+            if (maquina == "✊" and jugador == "✊") or (maquina == "🖐️" and jugador == "🖐️") or (maquina == "✌️" and jugador == "✌️"):
+
+                print("el 🙋‍♀️ ha sacado",jugador, " y la 🤖", maquina,"🤝\n")
+ 
+ # Muestra quién ha ganado la serie de 3 partidas
+
+        if partidas_ganadas_maquina > partidas_ganadas_jugador:
+            print("Ha ganado la 🤖\n")
+
         else:
-            print("No has elegido opción correcta ❌❌")
-    
-            continue  # Salta el resto del bucle y vuelve a pedir la entrada del jugador
+                
+            print("Ha ganado el 🙋‍♀️\n")
 
-        #La maquina elige su opción de forma aleatoria
+# Iniciamos la partida
+mi_partida = PiedraPapelTijera()
 
-        maquina = ("✊", "🖐️", "✌️")
-        maquina = random.choice(maquina)
+# variable para controlar si se debe volver a jugar
+volver_a_jugar = input("¿Quieres empezar a jugar(s) o no(n)?")
 
-#Hacemos las comparativas para ver quien ha ganado
+os.system("clear")
+# Bucle principal que se ejecuta mientras el jugador quiera volver a jugar
+while volver_a_jugar == 's':
 
-#Las opciones donde el jugador gana:
+    mi_partida.jugar_partida()
 
-        if (maquina == "✊" and jugador == "🖐️") or (maquina == "🖐️" and jugador == "✌️") or (maquina == "✌️" and jugador == "✊"):
-            
-            partidas_ganadas_jugador = partidas_ganadas_jugador +1 # Incrementa el contador de partidas ganadas del jugador
-
-            print("el 🧟‍♀️ ha sacado",jugador, "y la 🤖", maquina,"ha ganado el 🧟‍♀️")       
-
-#Las opciones donde la máquina gana:
-
-        if (maquina == "✊" and jugador == "✌️") or (maquina == "🖐️" and jugador == "✌️") or (maquina == "✌️" and jugador == "🖐️"):    
-            
-            partidas_ganadas_maquina = partidas_ganadas_maquina +1 # Incrementa el contador de partidas ganadas de la máquina
-
-            print("el 🧟‍♀️ ha sacado",jugador, "y la 🤖",maquina,"ha ganado la 🤖")
-            
-#Las opciones donde la máquina y el jugador empatan:
-
-        if (maquina == "✊" and jugador == "✊") or (maquina == "🖐️" and jugador == "🖐️") or (maquina == "✌️" and jugador == "✌️"):
-
-            print("el 🧟‍♀️ ha sacado",jugador, "y la 🤖", maquina,"🤝")
-
-  # Muestra quién ha ganado la serie de 3 partidas
-
-    if partidas_ganadas_maquina > partidas_ganadas_jugador:
-            print("Ha ganado la 🤖")
-
-    else:
-        
-        print("Ha ganado el 🧟‍♀️")
-
+ 
 # Pregunta al jugador si quiere volver a jugar
     volver_a_jugar = input("¿Quieres volver a jugar(s) o no(n)?")
-
+    os.system("clear")
 # Mensaje de despedida
-print("Hasta la próxima 👋👋")
+print("Hasta la próxima 👋👋\n")
